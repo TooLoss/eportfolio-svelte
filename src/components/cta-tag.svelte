3cb612d2c177c0bb1a50308936d8855b0846fedb
Bilèle <bilele.elhaddadi@gmail.com>
2025-10-09 22:40:19 +0200

    feat: setup project

---
+++ src/components/cta-tag.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let label = 'Button';
+  export let iconSrc = '';
+</script>
+
+<button class="cta-button">
+  {#if iconSrc}
+    <img src={iconSrc} class="cta-button-icon" alt="{label} icon" />
+  {/if}
+  <h6>{label}</h6>
+</button>
+
+<style>
+  .cta-button {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    font-family: "Inter";
+    font-size: var(--text-tag);
+    background-color: var(--color-bg-2);
+    color: var(--color-text);
+    border-radius: 7pt;
+    border: 1px solid var(--color-border);
+    padding: 0.5rem 1rem;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
+    transition: background-color 0.2s, box-shadow 0.2s;
+  }
+
+  span {
+    font-family: "Inter";
+    font-size: var(--text-tag);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import ctabutton from '$components/cta-tag.svelte';
+  import githubIcon from '$lib/assets/github-logo.svg'; 
+</script>
+
+
+
+<ctabutton 
+  label="View Source" 
+  iconSrc={githubIcon}
+/>
+
+<style>
+	@import "../app.css"
+</style>
